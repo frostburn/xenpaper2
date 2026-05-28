b9c48f156b2d9dd8fbd5fd76dc32b288f8b814e9
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-05-28 22:12:05 +0300

    Move embed toolbar styles into component

--- src/App.vue
+++ src/App.vue
@@ -562,35 +562,6 @@ onUnmounted(() => {
   z-index: 4;
 }
 
-.actions-embed {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  flex: 0 0 auto;
-  flex-direction: row;
-  width: 100%;
-  padding-top: 0;
-}
-
-.actions-embed .action-button {
-  width: auto;
-  height: 3rem;
-  padding: 1rem 0.5rem;
-  font-size: 0.9rem;
-  line-height: 1rem;
-}
-
-.actions-embed .edit-link {
-  margin-left: auto;
-}
-
-.actions-embed .play-pause-button {
-  width: 3rem;
-  height: 3rem;
-  padding: 1rem 0.5rem;
-}
-
 .xenpaper-app-embed {
   padding-top: 3rem;
 }
