2e8d76ff361527378de01c0dc37a5922e8a8a2f8
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-06-23 19:19:28 +0300

    Support count in (grace:) too

--- src/components/NewInV2Sidebar.vue
+++ src/components/NewInV2Sidebar.vue
@@ -102,9 +102,9 @@ const newInV2Sections: SidebarSection[] = [
         description:
           'Use a grace setter before a note to make that note borrow a short duration from the following note. Repeat the ? marker to apply the same grace duration to multiple notes.',
         tune: `(bpm:90)(env:1624)
-(8?) D C- (16?) E D-
-(8??) B A G--
-(grace:4) G A--`,
+(8?) 5 3- (16?) 7 5-
+(8??) 7 10 8--
+(grace:4) 10 12--`,
       },
     ],
   },
